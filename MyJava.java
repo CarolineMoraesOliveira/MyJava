fa918f057a1770f8924f417477b5d753f96cc888
Caroline M Oliveira <carolinemo.dev@gmail.com>
2023-07-31 15:06:22 -0300

    My first java file

---
+++ MyJava.java
@@ -0,0 +1,7 @@
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("My name is Caroline");
+        System.out.println("I am 40 years old");
+        System.out.println("My hometown is Cuiabá - Mato Grosso state - Brazil!");
+    }
+}
